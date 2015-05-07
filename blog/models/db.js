3bf5882e598bc2d9093f053260e951b10b6a1222
wrj913 <wrj913@gmail.com>
2015-05-07 18:30:19 +0800

    添加 express blog

---
+++ blog/models/db.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wrj on 2015/5/7.
+ */
+var settings = require('../settings'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
+module.exports = new Db(settings.db, new Server(settings.host, settings.port),
+    {safe: true});
---
+++ blog/settings.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wrj on 2015/5/7.
+ */
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'blog',
+    host: 'localhost',
+    port: 27017
+};
